fc465afaef7a44d1cb2c5c7564a39a9dd58dd133
yacine-hammouche <32833027+yacine-hammouche@users.noreply.github.com>
2017-11-13 17:48:24 +0100

    Update form-jquery-validation.js

--- tp3/form-jquery-validation.js
+++ tp3/form-jquery-validation.js
@@ -1,12 +1,23 @@
-$( document ).ready(function() {
-   // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
-   // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-    console.log( "DOM ready!" );
-    $("#submit").on("click",function(event){
-      event.preventDefault();
-      console.log( "click" );
-      $('#myModal').modal("show");
-    });
-    
-    // Y mettre le code jQuery pour valider tous les champs du formulaire
-});
+$(document).ready(function () { //pour chargement de la page
+   
+       $('#button').on("click", function (event) { //ajouter un "listener" à un élément
+           event.preventDefault();
+           console.log( "click" );
+   
+           if ($('#inputNom').val() == "" || 
+		$('#inputprenom').val() == "" || 
+		$('#inputdatedenaissance').val() == "" || 
+		$('#inputAdresse').val() == "" || 
+		$('#InputEmail').val() == "") {
+   
+               $('#myModal').modal("show");
+           }
+           else {
+               $(".modal-body").html('<p>Vous etes nés le '+$('#inputdatedenaissance').val()+'</p> <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#inputAdresse').val()+'&markers='+$('#inputAdresse').val()+'&size=800x400&zoom=13"/>');
+               $(".modal-title").text("Bienvenue "+$("#inputNom").val());
+               $('#myModal').modal("show");
+   
+           }
+   
+       });
+   })
